469f1a01e7622124994ced2d2d2298c2bff0bc9a
Stephen Wong <stephen.hlx+github@gmail.com>
2024-05-05 20:49:41 -0700

    Make main() take input from CLI and process the file
    Due to time limit there is no proper CLI implementation here.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::{env, fs::File, io::BufReader, sync::Arc};
+
+use dashmap::DashMap;
+use jouet_paiement::account::SimpleAccountTransactor;
+use jouet_paiement::model::{AccountSummary, AccountSummaryCsvWriter};
+use jouet_paiement::transaction_processor::SimpleTransactionProcessor;
+use jouet_paiement::transaction_stream_processor::async_csv_stream_processor::AsyncCsvStreamProcessor;
+use jouet_paiement::transaction_stream_processor::TransactionStreamProcessor;
 #[cfg(test)]
 use rstest_reuse;
 
@@ -6,4 +14,30 @@ mod model;
 mod transaction_processor;
 mod transaction_stream_processor;
 
-fn main() {}
+#[tokio::main]
+async fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename = args.get(1).unwrap();
+    let result = process_file(filename).await;
+    println!("{result}");
+}
+
+async fn process_file(filename: &str) -> String {
+    let accounts = Arc::new(DashMap::new());
+    let account_transaction_processor = SimpleAccountTransactor::new();
+    let transaction_processor =
+        SimpleTransactionProcessor::new(accounts.clone(), Box::new(account_transaction_processor));
+    let senders_and_handles = DashMap::new();
+
+    let processor =
+        AsyncCsvStreamProcessor::new(Arc::new(transaction_processor), senders_and_handles);
+    let file = File::open(filename).unwrap();
+
+    processor.process(BufReader::new(file)).await.unwrap();
+    processor.shutdown().await.unwrap();
+    let summaries: Vec<AccountSummary> = accounts
+        .iter()
+        .map(|entry| AccountSummary::from(entry.value()))
+        .collect();
+    String::from_utf8(AccountSummaryCsvWriter::write(summaries).unwrap()).unwrap()
+}
--- tests/e2e.rs
+++ tests/e2e.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::{fs::File, io::BufReader, sync::Arc};
 
 use dashmap::DashMap;
 use jouet_paiement::{
@@ -21,13 +21,10 @@ async fn e2e_small_input_using_async_processor() {
     let processor =
         AsyncCsvStreamProcessor::new(Arc::new(transaction_processor), senders_and_handles);
 
-    let input = "
-       type, client, tx, amount
-    deposit,      1, 10,    4.0
-    deposit,      1, 20,    5.0
-    deposit,      2, 30,    6.0";
+    let file = File::open("tests/small_input.txt").unwrap();
+    let reader = BufReader::new(file);
 
-    processor.process(input.as_bytes()).await.unwrap();
+    processor.process(reader).await.unwrap();
     processor.shutdown().await.unwrap();
 
     let mut summaries: Vec<AccountSummary> =
